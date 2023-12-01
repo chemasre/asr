b777b8da4022e2966aea56d4606e4aab428d5517
José Manuel (Chema) Solís <72490746+chemasre@users.noreply.github.com>
2023-12-01 22:55:32 +0100

    Add files via upload
    + Ground distance effect.

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
    
 **************************************************************/
 
-#define SCREEN_HEIGHT 20
-#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 29
+#define SCREEN_WIDTH 119
 
 #define SCREEN_FPS 10
 
@@ -22,10 +22,15 @@
 #define PI 3.1415927
 
 #define FOV 60.0f
+#define MAX_FOV 60.0f
+#define MIN_FOV 30.0f
+#define FOV_STEPS 10
 
-#define DISTANCE_STEPS 10
+#define WALL_STEPS 10
+#define GROUND_STEPS 3
 
-char distanceSteps[DISTANCE_STEPS + 1] = "WKMOoL|li.";
+char wallSteps[WALL_STEPS + 1] = "WKMOoL|li-";
+char groundSteps[GROUND_STEPS + 1] = "~-.";
 
 char screen[SCREEN_HEIGHT][SCREEN_WIDTH + 1];
 
@@ -52,8 +57,9 @@ float rayStep = 0.1f;
 float rotateStep = 5.0f;
 float moveStep = 0.2f;
 float viewDistance = 5.0f;
+float fov;
 
-HANDLE hStdout;
+HANDLE consoleHandle;
 
 char linea[MAP_WIDTH + 1];
 
@@ -137,13 +143,31 @@ void drawString(char s[], int x, int y)
     }
 }
 
-void drawColumn(int x, int h, char c)
+void drawColumn(int x, float wallDistanceFactor)
 {
+    int h = SCREEN_HEIGHT * (1 - wallDistanceFactor);
+    
+    int wallStep = (int)(wallDistanceFactor * WALL_STEPS);
+    if(wallStep >= WALL_STEPS) { wallStep = WALL_STEPS - 1; }
+    
+    char w = wallSteps[wallStep];
+    
     for(int y = 0; y < SCREEN_HEIGHT; y ++)
     {
         if(y <= SCREEN_HEIGHT/2 + h/2 && y >= SCREEN_HEIGHT/2 - h/2)
         {
-            screen[y][x] = c; 
+            screen[y][x] = w; 
+        }
+        else if(y > SCREEN_HEIGHT/2 + h/2)  
+        {
+            float groundDistanceFactor = 1 - (y - (SCREEN_HEIGHT/2.0f)) / (SCREEN_HEIGHT/2.0f);
+            int groundStep = (int)(groundDistanceFactor * GROUND_STEPS);
+            if(groundStep >= GROUND_STEPS) { groundStep = GROUND_STEPS - 1; }
+        
+            char g = groundSteps[groundStep];
+            
+            screen[y][x] = g;
+            
         }
     }
 }
@@ -153,17 +177,21 @@ void drawView()
     float centerX = (float)SCREEN_WIDTH / 2;
     
     for(int x = 0; x < SCREEN_WIDTH; x ++)
-    {
-        float distance = rayCast(posX, posY, angle + (x - centerX) / (float)SCREEN_WIDTH * FOV / 2, rayStep, viewDistance);
+    {        
+        float distanceFactor;
         
-        if(distance >= 0)
+        float distance = rayCast(posX, posY, angle + (x - centerX) / (float)SCREEN_WIDTH * fov / 2, rayStep, viewDistance);
+
+        if(distance < 0)
         {
-            float distanceFactor = distance / viewDistance;
-            int distanceStep = (int)(distanceFactor * DISTANCE_STEPS);
-            if(distanceStep >= DISTANCE_STEPS) { distanceStep = DISTANCE_STEPS - 1; }
-            
-            drawColumn(x, SCREEN_HEIGHT * (1 - distanceFactor), distanceSteps[distanceStep]);
+            distanceFactor = 1;
         }
+        else
+        {
+            distanceFactor = distance / viewDistance;
+        }
+        
+        drawColumn(x, distanceFactor);
     }
 }
 
@@ -172,7 +200,7 @@ void setScreenCursorPosition(int x, int y)
     COORD coord;    
     coord.X = 0;
     coord.Y = y;
-    SetConsoleCursorPosition(hStdout, coord);    
+    SetConsoleCursorPosition(consoleHandle, coord);    
 }
 
 void clearScreen()
@@ -201,41 +229,69 @@ void showScreen()
 
 void updatePlayer()
 {
-    int forward = isKeyPressed('W');
-    int backwards = isKeyPressed('S');
+    int strafe = isKeyPressed(VK_MENU);
+    int moveForward = isKeyPressed('W');
+    int moveBackwards = isKeyPressed('S');
     int left = isKeyPressed('A');
-    int right = isKeyPressed('D');
-    
-    if(left)
+    int right = isKeyPressed('D');    
+    int rotateLeft = left && !strafe;
+    int rotateRight = right && !strafe;
+    int moveLeft = left && strafe;
+    int moveRight = right && strafe;
+
+    if(rotateLeft)
     {
         angle -= rotateStep;
     }
-    else if(right)
+    else if(rotateRight)
     {
         angle += rotateStep;
     }
     
-    if(forward || backwards)
+    if(moveForward || moveBackwards || moveLeft || moveRight)
     {
-        float stepX = cos(angle * DEG2RAD) * moveStep;
-        float stepY = sin(angle * DEG2RAD) * moveStep;
         
         float nextPosX;
         float nextPosY;
 
         nextPosX = posX;
         nextPosY = posY;
+        
+        if(moveForward || moveBackwards)
+        {
+            float stepForwardX = cos(angle * DEG2RAD) * moveStep;
+            float stepForwardY = sin(angle * DEG2RAD) * moveStep;
 
-        if(forward)
-        {
-            nextPosX += stepX;
-            nextPosY += stepY;
+            if(moveForward)
+            {
+                nextPosX += stepForwardX;
+                nextPosY += stepForwardY;
+            }
+            else
+            {
+                nextPosX -= stepForwardX;
+                nextPosY -= stepForwardY;
+            }
         }
-        else
+
+        
+        if(moveLeft || moveRight)
         {
-            nextPosX -= stepX;
-            nextPosY -= stepY;
+            float stepLateralX = cos((angle + 90) * DEG2RAD) * moveStep;
+            float stepLateralY = sin((angle + 90) * DEG2RAD) * moveStep;
+            
+            if(moveRight)
+            {
+                nextPosX += stepLateralX;
+                nextPosY += stepLateralY;
+            }
+            else
+            {
+                nextPosX -= stepLateralX;
+                nextPosY -= stepLateralY;
+            }            
         }
+        
         
         if(nextPosX < 0) { nextPosX = 0; }
         else if(nextPosX > MAP_WIDTH) { nextPosX = MAP_WIDTH; }
@@ -251,6 +307,28 @@ void updatePlayer()
 
     }
     
+    int increaseFov = isKeyPressed('1');
+    int decreaseFov = isKeyPressed('2');
+    
+    if(increaseFov || decreaseFov)
+    {
+        float fovStep = (MAX_FOV - MIN_FOV) / FOV_STEPS;
+        
+        if(increaseFov)
+        {
+            fov = fov + fovStep;
+        }
+        else
+        {
+            fov = fov - fovStep;
+        }
+        
+        if(fov < MIN_FOV) { fov = MIN_FOV; }
+        else if(fov > MAX_FOV) { fov = MAX_FOV; }
+        
+    }
+    
+    
         
 }
 
@@ -260,11 +338,13 @@ void initPlayer()
     posY = 9.5f;
     
     angle = -90.0f;
+    
+    fov = MIN_FOV;
 }
 
 void initScreen()
 {
-    hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 }
 
 
@@ -314,7 +394,7 @@ void main()
         
         Sleep((int)(1.0f / SCREEN_FPS * 1000));
         
-    }    
-    
+    }   
+
 
 }
