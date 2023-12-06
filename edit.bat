da7a8c49191e520f776479800f7b8c5b7208681b
chemasre <chema.sre@gmail.com>
2023-12-06 14:39:21 +0100

    + Improved build files

--- edit.bat
+++ edit.bat
@@ -1,2 +1,3 @@
 set PATH=%PATH%;C:\Program Files\Notepad++
-notepad++ -openFoldersAsWorkspace .
+start notepad++ -openFoldersAsWorkspace .
+%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
